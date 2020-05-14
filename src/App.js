f6ffbb2c59ce3cc9fd37449fc1e6278be1b839cb
Bianca Belinda <belindabee42@gmail.com>
2020-05-14 16:59:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import about from "./component/about"
+import background from "./component/background"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={about} />
+      <Route path="background" component={background} />
+    </Router>
   );
 }
 
---
+++ src/component/about.jsx
@@ -0,0 +1,167 @@
+import React from "react";
+import layers from "../img/layers.png";
+import cogwheel from "../img/cogwheel.png";
+import pen from "../img/pen.png";
+import browser from "../img/browser.png";
+import belinda from "../img/belinda.jpg";
+import PropTypes from "prop-types";
+
+export default class About extends React.Component {
+	render() {
+		return (
+			<div
+				id="about-container"
+				className="content-containers container text-center mt-5">
+				<h1 id="about" className={this.props.bounceLeft}>
+					About
+				</h1>
+				<div className="row mt-5">
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInLeft
+						}>
+						<img src={layers} alt=""/>
+						<h4>Responsive</h4>
+					</div>
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInLeft
+						}>
+						<img src={pen} alt=""/>
+						<h4>Design</h4>
+					</div>
+					<div
+						className={
+							"col-6 col-sm-6 col-md-3 " + this.props.fadeInRight
+						}>
+						<img src={cogwheel} alt=""/>
+						<h4>Performance</h4>
+					</div>
+					
+				</div>
+
+				<div className="row" style={{ marginTop: 7 + "rem" }}>
+					<div className="col-12 col-lg-6 hidden">
+						<img
+							className={"img-fluid " + this.props.fadeIn}
+							src={belinda}
+							alt=""
+							style={{
+								borderRadius: 50 + "%",
+								height: 250 + "px",
+								width: 250 + "px"
+							}}
+						/>
+						<p className={"mytext pt-3 " + this.props.fadeIn}>
+						Hello. I am a self-taught front-end developer. I primarily focus on wrting clean, elegant, and efficient code. I am proficient in HTML, CSS, SCSS/SASS, REACT-REDUX, NODEJS. I also review existing code and consult with you on how to improve it.
+						</p>
+					</div>
+					<div
+						className={
+							"col-12 col-sm-12 col-md-12 col-lg-6 " +
+							this.props.tada
+						}>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								HTML5
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 80 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								CSS3
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-90-bar"
+								role="progressbar"
+								style={{ width: 70 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								JAVASCRIPT
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 50 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								PHP
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 70 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								REACT.JS
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 65 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								LARAVEL		
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 60 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								SASS
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-80-bar"
+								role="progressbar"
+								style={{ width: 70 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								LINUX
+							</div>
+						</div>
+						<div className="progress mb-3">
+							<div
+								className="progress-bar fill-60-bar"
+								role="progressbar"
+								style={{ width: 50 + "%" }}
+								aria-valuemin="0"
+								aria-valuemax="100">
+								ASP.NET
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+About.propTypes = {
+	bounceLeft: PropTypes.string,
+	fadeInLeft: PropTypes.string,
+	fadeInRight: PropTypes.string,
+	fadeIn: PropTypes.string,
+	tada: PropTypes.string
+};
